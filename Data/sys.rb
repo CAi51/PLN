21c84dd047a47612a9d7dd25abd892317a089ffa
CAi51 <GamerCAi51@outlook.com>
2017-05-21 15:27:49 +0800

    005

--- Data/sys.rb
+++ Data/sys.rb
@@ -2,11 +2,13 @@ class Main
     def initialize
         @name = "Trmp"
         @age = 3
+        @tool = "VSC"
         main
     end
     def main 
         puts "Hello World!#{@name}"
         puts "My age is #{@age}"
+        puts "I use #{@tool}"
         $scene = nil
     end
 end
