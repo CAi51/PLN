db9cbf73caeaac5739f5bb627ba63c55aa19c192
CAi51 <GamerCAi51@outlook.com>
2017-08-15 14:29:12 +0800

    2017-08-15

--- Data/sys.rb
+++ Data/sys.rb
@@ -1,6 +1,9 @@
 class Main
     def initialize
-        main
+        loop do
+            main
+            break if $scene != self
+        end
     end
     def main 
         puts "Hello World!"
